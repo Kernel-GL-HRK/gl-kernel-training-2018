a855480360db3d5601cd33e347c5b686ba6a9160
Illia Iorin <illia.iorin@gloaballogic.com>
2018-10-16 01:29:34 +0300

    Lesson 2/game.c: Add input identification
    Use switch as the best way for simple parse.
    
    Signed-off-by: Illia Iorin <illia.iorin@gloaballogic.com>

--- 02-Git/game.c
+++ 02-Git/game.c
@@ -2,5 +2,26 @@
 
 int main()
 {
+	char answer=0;
+	printf("Please choose: rock (r) - paper (p) - scissors (s)\n");
+	scanf("%c",&answer);
+	char * player;
+	switch (answer)
+	{
+		case 'r':
+			player = "rock";
+			break;
+		case 'p':
+			player = "paper";
+			break;
+		case 's':
+			player = "scissors";
+			break;
+		default:
+			printf("Error char\n");
+			return 1;
+	}
+	printf("You choose %s, I choose rock\n",player);
+	printf("I win: paper beats rock\n");
 	return 0;
 }
