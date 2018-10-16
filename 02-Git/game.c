d8c306cded82b13b4c1cf2a0a00a090297bddeaa
Illia Iorin <illia.iorin@gloaballogic.com>
2018-10-16 18:55:06 +0300

    Lesson 2/game.c: Implement game logic
    Removed the plugs. Implementing  bases on switch and rand.
    Game algorithm is:
    -Program choice, who will win.
    -Player choice the weapon.
    -Program choice needed weapon.
    
    Signed-off-by: Illia Iorin <illia.iorin@gloaballogic.com>

--- 02-Git/game.c
+++ 02-Git/game.c
@@ -1,8 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void result (char weapon, _Bool win)
+{
+	char * output;
+	if(win)
+	{
+		switch (weapon)
+		{
+			case 'r':
+				printf(" I choose scissors\n");
+				output = "You win: rock beats scissors\n";
+				break;
+			case 'p':
+				printf(" I choose rock\n");
+				output = "You win: paper beats rock\n";
+				break;
+			case 's':
+				printf(" I choose paper\n");
+				output = "You win: scissors beats paper\n";
+				break;
+		}
+	}
+	else
+	{
+		switch (weapon)
+		{
+			case 's':
+				printf(" I choose rock\n");
+				output = "I win: rock beats scissors\n";
+				break;
+			case 'r':
+				printf(" I choose paper\n");
+				output = "I win: paper beats rock\n";
+				break;
+			case 'p':
+				printf(" I choose scissors\n");
+				output = "I win: scissors beats paper\n";
+				break;
+		}
+	}
+	printf(output);
+}
 
 int main()
 {
+	time_t t;
+	srand((unsigned) time(&t));
 	char answer=0;
+	_Bool win = rand()%2;
+
 	printf("Please choose: rock (r) - paper (p) - scissors (s)\n");
 	scanf("%c",&answer);
 	char * player;
@@ -21,7 +68,7 @@ int main()
 			printf("Error char\n");
 			return 1;
 	}
-	printf("You choose %s, I choose rock\n",player);
-	printf("I win: paper beats rock\n");
+	printf("You choose %s,",player);
+	result(answer, win);
 	return 0;
 }
