b712398317e75c91b7951512461ea274a2f467d7
Dmitriy Ganshin <d.hanshyn@gmail.com>
2018-10-16 05:43:46 +0300

    02-Git: Add function read_user for user input
    Signed-off-by: Dmitriy Ganshin <d.hanshyn@gmail.com>

--- 02-Git/homework.c
+++ 02-Git/homework.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+char read_user(){
+	printf("Please choose: rock (r) - paper (p) - scissors (s)\n");
+	char user;
+	while(user != 'r' && user != 'p' && user != 's')
+		scanf("%c", &user);
+
+	return user;
+} 
+
 int main(){
+	char user = read_user();
+
 	return 0;	
 }
