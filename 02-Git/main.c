4c37838e3d0e34c37c25e12ebfa63716ae4b5504
KostantineFelix <constantineem@ukr.net>
2018-10-17 17:24:38 +0300

    lesson 2: Add game constants
    Signed-off-by: KostantineFelix <constantineem@ukr.net>

--- 02-Git/main.c
+++ 02-Git/main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+enum userChoice
+{
+        EPAPER,
+        ROCK,
+        SCISSORS,
+        FINISHGAME
+};
+
+enum winner
+{
+        USER,
+        COMPUTER,
+        BOTH
+};
+
 int main(void)
 {
 
