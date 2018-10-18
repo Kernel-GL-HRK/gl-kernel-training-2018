cbc22f74b804e7d4af334a306b8c9b48e568c8aa
ovdiyuk <ovdiyuk@gmail.com>
2018-10-18 13:22:26 +0000

    02-Git: File structure only

--- 02-Git/game.c
+++ 02-Git/game.c
@@ -13,15 +13,15 @@ int rndNum = random()%max+0;
 char rtnSmbl = strLine[rndNum];
 //cout << "rtnSmbl= " << rtnSmbl << "\n"; //Debug line
 
-while (userSmbl !='r' && userSmbl !='p' && userSmbl !='s')
 {
+while (userSmbl !='r' && userSmbl !='p' && userSmbl !='s')
 	cout << "Please choose: rock(r) - paper(p) - scissors (s) \n";
 	try {
 		cin >> userSmbl;
 	}
 	catch (int e) {
-		cout << "incorrect input. Error: " << e << "\n";
 	}
+		cout << "incorrect input. Error: " << e << "\n";
 }
 
 char concatSmbl[2];
@@ -32,8 +32,8 @@ char winLine[] = "rr-pp-ss-ps-sr-rp-sp-rs-sp"; //0..7 - Friends won;9..16 - Play
 int position;
 int winLineSize = sizeof(winLine);
 //cout << "winLineSize= " << winLineSize << "\n";  //Debug line
-
 for (int i =0; i < winLineSize; i++) {
+
 	if (rtnSmbl == winLine[i] && userSmbl == winLine[i+1]) {
 		position = i;
 		break;
@@ -42,12 +42,12 @@ for (int i =0; i < winLineSize; i++) {
 
 //cout << "position= " << position << "\n";  //Debug line
 cout << "I choose : " << rtnSmbl << ", You choose: " << userSmbl << "\n=======\n";
-
 if (position < 8) {
+
 	cout << "Friends win :|\n";
 }
-
 if (position > 8 && position < 17) {
+
 	cout << "You win :) \n";
 }
 if (position > 17) {
