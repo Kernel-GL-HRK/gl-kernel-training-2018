3f80237aea13ab017c2fdd580ab1ac85cd68a23b
Dmitriy Ganshin <d.hanshyn@gmail.com>
2018-10-18 04:29:35 +0300

    02-Git: Add resul function  to display game result
    Signed-off-by: Dmitriy Ganshin <d.hanshyn@gmail.com>

--- 02-Git/homework.c
+++ 02-Git/homework.c
@@ -22,11 +22,38 @@ char rnd()
 	return arr[rnd];
 }
 
+char *conver_char(char c)
+{
+	if (c == 'r') return "rock";       
+	if (c == 's') return "scissors";       
+	if (c == 'p') return "paper"; 
+}
+
+void result(char user, char comp)
+{
+	
+	printf("You choose %s, I choose %s\n", 
+		conver_char(user), conver_char(comp));
+		
+	if(user == comp)
+		printf("Draw:\n");
+		else if ((user == 'r') && (comp == 's') ||
+			(user == 's') && (comp == 'p') 	||
+			(user == 'p') && (comp == 'r'))	
+				printf("You Win: %s beats %s\n",
+					conver_char(user), conver_char(comp));
+			else	
+				printf("You Lose: %s beats %s\n",
+					conver_char(comp), conver_char(user));				
+}
+
 int main()
 {
 
 	char user = read_user();
 	char comp = rnd();
+
+	result(user, comp);
 		
 	return 0;	
 }
