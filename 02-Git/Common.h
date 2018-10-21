b1d716a98c173ee9832e561dc5fd1343501aa68b
Oleksandr Aleksieiev <ziod.alx@gmail.com>
2018-10-21 22:16:41 +0300

    02-Git: Implement Driver, Verbose and Move
    Signed-off-by: Oleksandr Aleksieiev <ziod.alx@gmail.com>

---
+++ 02-Git/Common.h
@@ -0,0 +1,14 @@
+#ifndef LESSON02_GIT_COMMON_H
+#define LESSON02_GIT_COMMON_H
+
+#define _DBG_PRINT_ 1
+
+#ifdef _DBG_PRINT_
+#include <cstdio> // getchar
+
+	#define DbgPrintI32(_x_) printf("DbgPrint: %d\n", (_x_));
+#else
+	#define DbgPrintI32(_x_)
+#endif
+
+#endif
