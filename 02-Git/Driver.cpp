451f399a23fa5e4fb0f271a4b4d76a4fd1d5f02a
Oleksandr Aleksieiev <ziod.alx@gmail.com>
2018-10-21 19:18:33 +0300

    02-Git: Implement Driver constructor

--- 02-Git/Driver.cpp
+++ 02-Git/Driver.cpp
@@ -2,14 +2,28 @@
  * Implements game core
  */
 #include "Driver.h"
+#include "Move.h"
 
 namespace RockPaperScissors {
 
 	Driver::Driver() {
+		state[mvvRock][mvvRock] = grDraw;
+		state[mvvRock][mvvPaper] = grPlayer1Win;
+		state[mvvRock][mvvScissors] = grPlayer0Win;
 
+		state[mvvPaper][mvvRock] = grPlayer0Win;
+		state[mvvPaper][mvvPaper] = grDraw;
+		state[mvvPaper][mvvScissors] = grPlayer1Win;
+
+		state[mvvScissors][mvvRock] = grPlayer1Win;
+		state[mvvScissors][mvvPaper] = grPlayer0Win;
+		state[mvvScissors][mvvScissors] = grDraw;
 	}
 
 	Driver::~Driver() {
 
+	}
+
+	GameResult Driver::getGameResult(Move player0, Move player1) {
 	}
 }
