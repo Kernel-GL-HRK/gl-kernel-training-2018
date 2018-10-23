6187f6a073535528bc1aee0b9e826e344f662e51
Vadym Mishchuk <superoriginalmailboxname@gmail.com>
2018-10-24 00:52:54 +0300

    lesson03: Redirect HW info to file instead of stdout
    Signed-off-by: Vadym Mishchuk <superoriginalmailboxname@gmail.com>

--- 03-bash/task1.sh
+++ 03-bash/task1.sh
@@ -94,38 +94,35 @@ touch "$FILE_PATH"
 
 echo Use path "$FILE_PATH"
 
-exit 0
 
-
-echo "--------------Hardware---------------------"
-echo CPU: \"$(dmidecode -t 4 | grep Version | cut -d' ' -f2-)\"
+echo "--------------Hardware---------------------" >>$FILE_PATH
+echo CPU: \"$(dmidecode -t 4 | grep Version | cut -d' ' -f2-)\" >>$FILE_PATH
 echo RAM: $( sudo dmidecode --type memory | awk '
 /Size: [0-9]+ MB$/ { 
   total += $2; 
   } END { 
   print total 
-}') MB
-echo Motherboard: \"$(dmidecode -s baseboard-manufacturer)\", \"$(dmidecode -s baseboard-product-name)\"
-echo System Serial Number: $(dmidecode -s system-serial-number)
+}') MB >>$FILE_PATH
+echo Motherboard: \"$(dmidecode -s baseboard-manufacturer)\", \"$(dmidecode -s baseboard-product-name)\" >>$FILE_PATH
+echo System Serial Number: $(dmidecode -s system-serial-number) >>$FILE_PATH
 
-echo "--------------System-----------------------"
-echo $(lsb_release -a 2>/dev/null | grep Description | awk '{sub(/Description/,"OS Distribution",$0); print $0}')
-echo Kernel version: $(uname -r)
-echo Hostname: $(uname -n)
-echo Uptime: $(uptime | awk '{sub(/,/," ",$0); for (i=0; i<=NF; i++){if ($i == "up"){print $(i+1);exit}}}')
-echo Processes running: $(ps -A | wc -l)
-echo Users logged in: $(who | wc -l)
+echo "--------------System-----------------------" >>$FILE_PATH
+echo $(lsb_release -a 2>/dev/null | grep Description | awk '{sub(/Description/,"OS Distribution",$0); print $0}') >>$FILE_PATH
+echo Kernel version: $(uname -r) >>$FILE_PATH
+echo Hostname: $(uname -n) >>$FILE_PATH
+echo Uptime: $(uptime | awk '{sub(/,/," ",$0); for (i=0; i<=NF; i++){if ($i == "up"){print $(i+1);exit}}}') >>$FILE_PATH
+echo Processes running: $(ps -A | wc -l) >>$FILE_PATH
+echo Users logged in: $(who | wc -l) >>$FILE_PATH
 
-echo "--------------Network-----------------------"
+echo "--------------Network-----------------------" >>$FILE_PATH
 ip addr | awk '
 /^[0-9]+:/ {
   sub(/:/,"",$2); iface=$2  
 }
 /inet / { 
   print iface" : "$2""
-}'
-echo "-------------EOF----------------------------"
+}' >>$FILE_PATH
+echo "-------------EOF----------------------------" >>$FILE_PATH
 
 
-
-
+exit 0
