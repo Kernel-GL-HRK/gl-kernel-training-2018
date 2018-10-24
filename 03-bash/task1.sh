0a7ea32a6a70331d0df3615cbebc9528aebf3972
KostantineFelix <constantineem@ukr.net>
2018-10-24 22:47:00 +0300

    Bash homework: Add locale dependent help
    Signed-off-by: KostantineFelix <constantineem@ukr.net>

--- 03-bash/task1.sh
+++ 03-bash/task1.sh
@@ -2,11 +2,32 @@
 
 clear
 
-function print_help(){
-	  	echo "print help"
+function get_locale_name (){
+ 	local locale_name=$(locale | grep "LC_NAME=")
+ 	expr "$locale_name" : '\S*=\([^.]*\)'
 }
+
+function print_help(){	
+	echo "./task1.sh [-h|--help] [-n num] [file]"
+	if [[ $(get_locale_name) = "uk_UA" ]]
+	then
+		>&2 echo "де 'num' - кількість файлів із результатами,"
+		>&2 echo "'file' - шлях та ім'я файла, у який треба записати результат"
+		>&2 echo "наприклад: task1.sh -n 9 ./test/info/info.txt"
+	else
+		>&2 echo "where 'num' - number result files"
+		>&2 echo "'file' - path to and  result file name "
+		>&2 echo 'e.g. task1.sh -n 9 ./test/info/info.txt'
+	fi
+}
+
+
 num_files = "0"
 file_path =""
+
+cur_local=$(get_locale_name)
+
+
 #parse command line 
 #./task1.sh [-h|--help] [-n num] [file]
 while (( "$#" )); do 
