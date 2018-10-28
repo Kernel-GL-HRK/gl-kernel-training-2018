b811343036ef6a12dae7266384528fa92a46782d
Vadym Mishchuk <superoriginalmailboxname@gmail.com>
2018-10-28 22:17:16 +0200

    lesson04: Add parameter for setting module load result
    Signed-off-by: Vadym Mishchuk <superoriginalmailboxname@gmail.com>

--- 04-KernelModule/solution/solution.c
+++ 04-KernelModule/solution/solution.c
@@ -1,12 +1,20 @@
 #include <linux/init.h>      // Macros used to mark up functions  __init __exit
 #include <linux/module.h>    // Core header for loading LKMs into the kernel
 #include <linux/kernel.h>    // Contains types, macros, functions for the kernel
+#include <linux/moduleparam.h>
 
+static int will_return;
+
+module_param(will_return, int, 0400);
 
 static int __init hello_init(void)
 {
-	pr_info("Module loaded\n");
-	return 0;
+	if (will_return)
+		pr_err("Module load will fail\n");
+	else
+		pr_info("Module loaded\n");
+
+	return will_return;
 }
 
 static void __exit hello_exit(void)
