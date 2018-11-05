31b48f4b232e2fc4a2528ddc310aca416a008483
Oleksandr Siruk <alexsirook@gmail.com>
2018-11-05 16:24:31 +0200

    Lesson_05: Added read only stat for procfs module
    Signed-off-by: Oleksandr Siruk <alexsirook@gmail.com>

--- 05-Interfaces/05_procfs/simple_procfs.c
+++ 05-Interfaces/05_procfs/simple_procfs.c
@@ -8,6 +8,13 @@
 
 static char msg[PAGE_SIZE];
 static ssize_t msg_size;
+static char msg_stat[PAGE_SIZE];
+static ssize_t msg_stat_size;
+static int converted_cnt;
+static int processed_cnt;
+static int read_cnt;
+static int write_cnt;
+static char stat_update_flag;
 
 static ssize_t mywrite(struct file *file, const char __user *pbuf,
 size_t count, loff_t *ppos)
@@ -19,8 +26,10 @@ size_t count, loff_t *ppos)
 	not_copied = copy_from_user(msg, pbuf, count);
 
 	msg_size = count - not_copied;
-
+	processed_cnt += msg_size;
 	pr_info("simple_procfs: %s: msg_size=%d\n", __func__, msg_size);
+	write_cnt++;
+	stat_update_flag = 1;
 	return msg_size;
 }
 
@@ -36,16 +45,44 @@ size_t count, loff_t *ppos)
 	if (num) {
 		not_copied = copy_to_user(pbuf, msg, num);
 		num -= not_copied;
+		for (i = 0; i < num; i++) {
+			if (pbuf[i] <= (int)'z' && pbuf[i] >= (int)'a') {
+				pbuf[i] = (int)pbuf[i] - (int)'a' + (int)'A';
+				converted_cnt++;
+			}
+		}
 	}
-
-	for (i = 0; i < num; i++) {
-		if (pbuf[i] <= (int)'z' && pbuf[i] >= (int)'a')
-			pbuf[i] = (int)pbuf[i] - (int)'a' + (int)'A';
-	}
-
 	msg_size = 0; /* Indicate EOF on next read */
-
 	pr_info("simple_procfs: %s: return=%d\n", __func__, num);
+	read_cnt++;
+	return num;
+}
+
+static ssize_t myread_stat(struct file *file, char __user *pbuf,
+size_t count, loff_t *ppos)
+{
+	ssize_t num = 0, not_copied;
+
+	pr_info("simple_procfs_stat: %s: count=%d\n", __func__, count);
+	if (!stat_update_flag)
+		goto exit_point;
+	msg_stat_size = snprintf(msg_stat, PAGE_SIZE - 1,
+	"Converted count = %d\n", converted_cnt);
+	msg_stat_size += snprintf(msg_stat + msg_stat_size,
+	PAGE_SIZE - msg_stat_size - 1, "Processed count = %d\n", processed_cnt);
+	msg_stat_size += snprintf(msg_stat + msg_stat_size,
+	PAGE_SIZE - msg_stat_size - 1, "Read count = %d\n", read_cnt);
+	msg_stat_size += snprintf(msg_stat + msg_stat_size,
+	PAGE_SIZE - msg_stat_size - 1, "write count = %d\n\n", write_cnt);
+	num = min_t(ssize_t, msg_stat_size, count);
+	if (num) {
+		not_copied = copy_to_user(pbuf, msg_stat, num);
+		num -= not_copied;
+	}
+
+exit_point:
+	stat_update_flag = 0; // indicate EOF for next build
+	pr_info("simple_procfs_stat: %s: return=%d\n", __func__, num);
 	return num;
 }
 
@@ -56,14 +93,31 @@ static const struct file_operations myops = {
 	.write = mywrite,
 };
 
+static const struct file_operations myops_stat = {
+	.owner = THIS_MODULE,
+	.read = myread_stat,
+};
+
 static struct proc_dir_entry *ent;
+static struct proc_dir_entry *ent_stat;
 
 
 static int simple_procfs_init(void)
 {
+	converted_cnt = 0;
+	processed_cnt = 0;
+	read_cnt = 0;
+	write_cnt = 0;
+	stat_update_flag = 0;
 	ent = proc_create("simple_procfs", 0644, NULL, &myops);
 	if (ent == NULL) {
 		pr_err("simple_procfs: error creating procfs entry\n");
+		return -ENOMEM;
+	}
+
+	ent_stat = proc_create("simple_procfs_stat", 0444, NULL, &myops_stat);
+	if (ent_stat == NULL) {
+		pr_err("simple_procfs_stat: error creating procfs entry\n");
 		return -ENOMEM;
 	}
 
@@ -74,6 +128,7 @@ static int simple_procfs_init(void)
 static void simple_procfs_exit(void)
 {
 	proc_remove(ent);
+	proc_remove(ent_stat);
 
 	pr_info("simple_procfs: module exited\n");
 }
