3a1d417cf7ac6c9deed9a842d931caee06b2f6e3
alex <eelleekk@gmail.com>
2018-11-09 23:35:39 +0200

    Lesson 06: Implement time read functions
    Signed-off-by: alex <eelleekk@gmail.com>

--- 06-TimeManagement/timer.c
+++ 06-TimeManagement/timer.c
@@ -4,10 +4,10 @@
 #include <linux/kernel.h>
 #include <linux/errno.h>
 #include <linux/uaccess.h>
-#include <linux/slab.h>
 #include <linux/ctype.h>
 #include <linux/sysfs.h>
 #include <linux/device.h>
+#include <linux/timekeeping.h>
 
 #define MODULE_TAG			"Timer"
 #define CLASS_NAME			"Timer"
@@ -17,6 +17,8 @@ module_param(interval, ulong, 0444);
 MODULE_PARM_DESC(interval, "An interval in ms for periodic output in dmesg");
 static struct timer_list print_timer;
 
+static unsigned long last_read;
+static struct timeval last_abs_time;
 
 static void update_print_timer(unsigned long time_ms)
 {
@@ -39,19 +41,42 @@ static void update_print_timer(unsigned long time_ms)
 
 static void print_timer_fn(unsigned long data)
 {
-	pr_info("%s: This is a timer callback\n", MODULE_TAG);
+	struct timespec  uptime;
+
+	get_monotonic_boottime(&uptime);
+	pr_info("%s: System Uptime: %d sec\n", MODULE_TAG, uptime.tv_sec);
 
 	update_print_timer(interval);
 }
 
 static ssize_t last_read_show(struct class *class, struct class_attribute *attr, char *buf)
 {
-	return 0;
+	int cnt;
+	unsigned long delta;
+
+	delta = jiffies - last_read;
+	last_read = jiffies;
+	delta = jiffies_to_msecs(delta)/1000;
+
+	cnt = scnprintf(buf, PAGE_SIZE, "%ld\n", delta);
+
+	return cnt;
 }
 
 static ssize_t last_absolute_show(struct class *class, struct class_attribute *attr, char *buf)
 {
-	return 0;
+	struct tm local_time;
+	int cnt;
+
+	time64_to_tm(last_abs_time.tv_sec, sys_tz.tz_minuteswest * 60, &local_time);
+
+	cnt = scnprintf(buf, PAGE_SIZE, "%d/%02d/%02d %02d:%02d:%02d\n",
+		local_time.tm_year + 1900, local_time.tm_mon, local_time.tm_mday,
+		local_time.tm_hour, local_time.tm_min, local_time.tm_sec);
+
+	do_gettimeofday(&last_abs_time);
+
+	return cnt;
 }
 
 static ssize_t print_interval_show(struct class *class, struct class_attribute *attr, char *buf)
@@ -105,6 +130,12 @@ static int __init mod_init(void)
 
 	setup_timer(&print_timer, print_timer_fn, 0);
 	update_print_timer(interval);
+
+	//Set a zero point for last_read
+	last_read = jiffies;
+
+	//Set a zero point for last_abs_time
+	do_gettimeofday(&last_abs_time);
 
 	pr_info("%s: module loaded\n", MODULE_TAG);
 	return 0;
