acb68d0f2b8445044f4125264a1c6fdc18260967
KostantineFelix <constantineem@ukr.net>
2018-11-15 14:41:16 +0200

    Homework 04: Add module param
    Signed-off-by: KostantineFelix <constantineem@ukr.net>

--- 04-KernelModule/simple_kernel_module.c
+++ 04-KernelModule/simple_kernel_module.c
@@ -1,6 +1,7 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/errno.h>
 
 
 MODULE_LICENSE("GPL");
@@ -8,7 +9,13 @@ MODULE_AUTHOR("Konstantin E Felix");
 MODULE_DESCRIPTION("First simple linux kenel module.");
 MODULE_VERSION("0.1");
 
+static int skm_param = -1;
+module_param(skm_param, int, 0);
+
 static int __init first_module(void){
+		if(skm_param <0 || skm_param>5)
+			return -EINVAL;
+
 		printk(KERN_SOH"Run simple kernel module");
 		return 0;
 }
