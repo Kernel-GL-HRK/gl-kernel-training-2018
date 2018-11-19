d0e1369b5bac3fee5ee14d40a063f734e9abd989
alex <eelleekk@gmail.com>
2018-11-19 04:27:22 +0200

    Lesson 07: Implement Slab memory management
    Signed-off-by: alex <eelleekk@gmail.com>

--- 07-MemoryManagement/to_lower_case.c
+++ 07-MemoryManagement/to_lower_case.c
@@ -13,6 +13,10 @@
 
 #define CLASS_NAME			"case_converter"
 
+static unsigned long int mode;
+module_param(mode, ulong, 0664);
+MODULE_PARM_DESC(mode, "Memory allocation mode");
+
 struct module_statistic {
 	int use_cnt;
 	int chars_processed;
@@ -20,10 +24,15 @@ struct module_statistic {
 	int chars_alphabetic;
 	int chars_numeric;
 };
+struct module_statistic statistic;
 
-struct module_statistic *statistic;
 static char *msg_buffer;
 static int msg_size;
+
+#define CACHE_SIZE PAGE_SIZE
+/*Memmory allocation types*/
+#define SLAB_MODE 0
+static struct kmem_cache *slab_mem;
 
 
 static void convert_to_lowercase(void)
@@ -34,62 +43,86 @@ static void convert_to_lowercase(void)
 	for (i = 0; i < msg_size; i++) {
 		temp = tolower(msg_buffer[i]);
 		if (temp != msg_buffer[i]) {
-			statistic->chars_converted++;
+			statistic.chars_converted++;
 			msg_buffer[i] = temp;
 		}
 		if (isalpha(temp))
-			statistic->chars_alphabetic++;
+			statistic.chars_alphabetic++;
 		else if (isdigit(temp))
-			statistic->chars_numeric++;
+			statistic.chars_numeric++;
 	}
-	statistic->chars_processed += msg_size;
-	statistic->use_cnt++;
+	statistic.chars_processed += msg_size;
+	statistic.use_cnt++;
 }
 
 static ssize_t numeric_show(struct class *class, struct class_attribute *attr, char *buf)
 {
-	sprintf(buf, "%d\n", statistic->chars_numeric);
+	sprintf(buf, "%d\n", statistic.chars_numeric);
 
 	return strlen(buf);
 }
 
 static ssize_t alphabetic_show(struct class *class, struct class_attribute *attr, char *buf)
 {
-	sprintf(buf, "%d\n", statistic->chars_alphabetic);
+	sprintf(buf, "%d\n", statistic.chars_alphabetic);
 
 	return strlen(buf);
 }
 
 static ssize_t used_show(struct class *class, struct class_attribute *attr, char *buf)
 {
-	sprintf(buf, "%d\n", statistic->use_cnt);
+	sprintf(buf, "%d\n", statistic.use_cnt);
 
 	return strlen(buf);
 }
 
 static ssize_t converted_show(struct class *class, struct class_attribute *attr, char *buf)
 {
-	sprintf(buf, "%d\n", statistic->chars_converted);
+	sprintf(buf, "%d\n", statistic.chars_converted);
 
 	return strlen(buf);
 }
 
 static ssize_t processed_show(struct class *class, struct class_attribute *attr, char *buf)
 {
-	sprintf(buf, "%d\n", statistic->chars_processed);
+	sprintf(buf, "%d\n", statistic.chars_processed);
 
 	return strlen(buf);
 }
 
 static ssize_t buffer_show(struct class *class, struct class_attribute *attr, char *buf)
 {
-	memcpy(buf, msg_buffer, msg_size);
+	if (msg_buffer == NULL)
+		return 0;
+
+	switch (mode) {
+		case SLAB_MODE:
+			memcpy(buf, msg_buffer, msg_size);
+			kmem_cache_free(slab_mem, msg_buffer);
+			msg_buffer = NULL;
+			break;
+	
+		default:
+			break;
+	}	
+
 	return msg_size;
 }
 
 static ssize_t buffer_store(struct class *class, struct class_attribute *attr, const char *buf, size_t count)
 {
 	ssize_t ret_cnt;
+
+	switch (mode) {
+		case SLAB_MODE:
+			if (msg_buffer == NULL) {
+				msg_buffer = kmem_cache_alloc(slab_mem, GFP_KERNEL);
+			}
+			break;
+	
+		default:
+			break;
+	}
 
 	if (count < PAGE_SIZE) {
 		memcpy(msg_buffer, buf, count);
@@ -140,32 +173,40 @@ static int __init mod_init(void)
 		return ret;
 	}
 
-	msg_buffer = kzalloc(PAGE_SIZE, GFP_KERNEL);
-	if (msg_buffer == NULL) {
-		pr_err("%s: Memory allocation error\n", MODULE_TAG);
-		goto fail;
-	}
-
-	statistic = kzalloc(sizeof(struct module_statistic), GFP_KERNEL);
-	if (statistic == NULL) {
-		pr_err("%s: Memory allocation error\n", MODULE_TAG);
-		goto fail;
+	switch (mode) {
+		case SLAB_MODE:
+			slab_mem = kmem_cache_create(MODULE_TAG, CACHE_SIZE, 0, SLAB_HWCACHE_ALIGN | SLAB_POISON, NULL);
+			if (slab_mem == NULL)
+				goto fail;
+			break;
+		default:
+			break;
 	}
 
 	pr_info("%s: module loaded\n", MODULE_TAG);
 	return 0;
 
 fail:
-	kfree(msg_buffer);
-	kfree(statistic);
+	class_unregister(&module_class);
 	return -ENOMEM;
 }
 
 static void __exit mod_exit(void)
 {
 	class_unregister(&module_class);
-	kfree(msg_buffer);
-	kfree(statistic);
+
+	switch (mode) {
+		case SLAB_MODE:
+			if(msg_buffer != NULL) {
+				kmem_cache_free(slab_mem, msg_buffer);
+			}
+			break;
+	
+		default:
+			break;
+	}
+
+	if (slab_mem) kmem_cache_destroy(slab_mem);
 	pr_info("%s: module unloaded\n", MODULE_TAG);
 }
 
