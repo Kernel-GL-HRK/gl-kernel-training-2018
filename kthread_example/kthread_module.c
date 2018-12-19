c9191a7f50c46a3bb50e52fd3474fb96bab7679f
Vadym Mishchuk <superoriginalmailboxname@gmail.com>
2018-12-20 00:28:21 +0200

    kthread example: replace spinlock with rt-mutex
    Signed-off-by: Vadym Mishchuk <superoriginalmailboxname@gmail.com>

--- kthread_example/kthread_module.c
+++ kthread_example/kthread_module.c
@@ -6,21 +6,22 @@
 #include <linux/timer.h>
 #include <linux/delay.h>
 #include <linux/spinlock.h>
+#include <linux/rtmutex.h>
 
 #define N_THR 4
 
 static struct task_struct *threads[N_THR];
 
-DEFINE_SPINLOCK(critical_section);
+DEFINE_RT_MUTEX(critical_section);
 
 static int thread_fn(void *ptr)
 {
 
 	while (!kthread_should_stop()) {
 		pr_info("Kthread example: in thread %d\n", (int)ptr);
-                spin_lock(&critical_section);
-                msleep_interruptible(200);
-                spin_unlock(&critical_section);
+		rt_mutex_lock(&critical_section);
+		msleep_interruptible(200);
+		rt_mutex_unlock(&critical_section);
 		msleep_interruptible(800);
 	}
 
@@ -35,8 +36,8 @@ int thread_init(void)
 	pr_info("Kthread example: in init\n");
 
 	for (i = 0; i < N_THR; i++)
-		threads[i] = kthread_run(thread_fn, 
-                                (void *)i, thread_format, i);
+		threads[i] = kthread_run(thread_fn,
+				(void *)i, thread_format, i);
 
 	return 0;
 }
