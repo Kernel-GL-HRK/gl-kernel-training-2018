5b1034076eafb146f9e574dda3fb99f695abd4e9
Androsov Aleksandr <oleksandr.androsov@l1inc.com>
2018-12-20 14:24:41 +0200

    Lesson 10: Part 1. Implement creation of a thread in two ways.
    In order to show two different ways of creating a thread, we can use
    a CREATE_THREAD_EASY macro.
    
    Signed-off-by: Androsov Aleksandr <oleksandr.androsov@l1inc.com>

--- 10-ConcurrencyAndSynchronization/thread.c
+++ 10-ConcurrencyAndSynchronization/thread.c
@@ -3,18 +3,50 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/kthread.h>
+#include <linux/delay.h>
+
+#define MODULE_TAG "threadmodule"
+
+//Different ways to create a thread
+#define CREATE_THREAD_EASY
 
 static int param = 5;
 module_param(param, int, 0444);
 MODULE_PARM_DESC(param, "Delay time. Default 5 sec");
 
-
+static int thrad_func(void *data)
+{
+	ssleep(2);
+	return 0;
+}
 
 static int __init tmod_init(void)
 {
-	pr_debug("Loading the test module\n");
+	struct task_struct *task; 
+	char task_name[TASK_COMM_LEN];
+	static int tnum = 0;
+	pr_debug("Loading %s\n", MODULE_TAG);
 
-	pr_debug("Test module loaded\n");
+#ifdef CREATE_THREAD_EASY
+	task = kthread_run(thrad_func, NULL, "%d:thread_%d", current->pid, tnum++);	
+#else
+	task = kthread_create(thrad_func, NULL, "%d:thread_%d", current->pid, tnum++);
+	if(!IS_ERR(task)){
+		wake_up_process(task);
+	}
+#endif
+
+	if(IS_ERR(task)){
+		return PTR_ERR(task);
+	}
+
+	get_task_comm(task_name, task);
+	pr_debug("Task %s is running\n", task_name);
+
+	ssleep(3);
+
+	pr_debug("%s loaded\n", MODULE_TAG);
 	return 0;
 }
 
