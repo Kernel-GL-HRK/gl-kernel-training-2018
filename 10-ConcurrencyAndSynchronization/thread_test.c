2ccd00203cc250dd3804d6ee4be4d0749b2aeedc
Oleksandr Siruk <alexsirook@gmail.com>
2018-12-20 17:37:21 +0200

    Lesson_10: Implemented kernel module which start THREAD_NUMBER separate threads
    This module will start THREAD_NUMBER separate threads which have logging for
    once per second and will be stoped by parent thread after delay_sec using
    kthread_should_stop and kthread_stop
    
    Signed-off-by: Oleksandr Siruk <alexsirook@gmail.com>

--- 10-ConcurrencyAndSynchronization/thread_test.c
+++ 10-ConcurrencyAndSynchronization/thread_test.c
@@ -3,22 +3,23 @@
 #include <linux/delay.h>
 #include <linux/kthread.h>
 
+#define THREAD_NUMBER 5
 static int delay_sec = 5;
-static struct task_struct *thread_st;
+static int thread_num = THREAD_NUMBER;
+
+static struct task_struct *(thread_st[THREAD_NUMBER]);
 module_param(delay_sec, int, 0);
+
 
 static int thread_func(void *data)
 {
 	int total_sleep = 0;
 
-	while (1) {
-		if (total_sleep >= delay_sec)
-			break;
+	while (!kthread_should_stop()) {
 		pr_info("thread: child process [%d] is running for %d seconds\n",
 		current->pid, total_sleep);
 		ssleep(1);
 		total_sleep += 1;
-
 	}
 	pr_info("thread: child process [%d] is completed. total sleep = %d\n",
 	current->pid, total_sleep);
@@ -27,9 +28,14 @@ static int thread_func(void *data)
 
 int test_thread(void)
 {
+	int i = 0;
+
 	pr_info("thread: main process [%d] is running\n", current->pid);
-	thread_st = kthread_run(thread_func, NULL, "test_thread");
-	ssleep(delay_sec + 2);
+	for (i = 0; i < thread_num; i++)
+		thread_st[i] = kthread_run(thread_func, NULL, "test_thread");
+	ssleep(delay_sec);
+	for (i = 0; i < thread_num; i++)
+		kthread_stop(thread_st[i]);
 	pr_info("thread: main process [%d] is completed\n", current->pid);
 	return -1;
 }
