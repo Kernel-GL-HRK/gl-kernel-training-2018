9ab34b421dfea9f0b760f27520875f5e0c8ace5b
serega911 <pasechnuy.sergey@gmail.com>
2018-12-23 18:56:30 +0200

    10-ConcurrencyAndSynchronization: Hometask
    	2. Create kernel module which start N separate
    	   threads using kthread_create or kthread_run with
    	   logging once per second for each thread. Thread should
    	   be stoped from parent thread by kthread_stop and
    	   kthread_should_stop after T seconds
    
    Signed-off-by: serega911 <pasechnuy.sergey@gmail.com>

--- 10-ConcurrencyAndSynchronization/concurency.c
+++ 10-ConcurrencyAndSynchronization/concurency.c
@@ -2,13 +2,27 @@
 #include <linux/module.h>
 #include <linux/kthread.h>
 #include <linux/delay.h>
+#include <linux/vmalloc.h>
+#include <linux/timer.h>
 
 #define LOG(str) pr_info(__FILE__": "str"\n");
+
+static int N = 1;
+module_param(N, int, 0000);
+MODULE_PARM_DESC(N, "Threads count to be created");
+
+static int T = 10;
+module_param(T, int, 0000);
+MODULE_PARM_DESC(T, "Thread lifetime in seconds");
+
+struct timer_list timer;
+static struct task_struct **kthreads;
+static int stop_flag = 0;
 
 int th_fun(void *data)
 {
 	LOG("thread start");
-	while( !kthread_should_stop() ) {
+	while( !kthread_should_stop() && !stop_flag ) {
 		LOG("log");
 		msleep(1000);
 	}
@@ -16,19 +30,48 @@ int th_fun(void *data)
 	return 0;
 }
 
-static struct task_struct *kthread;
+static void start_threads(void)
+{
+	int i = 0;
+
+	kthreads = vmalloc(sizeof(struct task_struct*) * N);
+	for (i = 0; i < N; i++){
+		kthreads[i]= kthread_run(th_fun, NULL, "test_thread_%d", i);
+	}
+}
+
+static void stop_threads(void)
+{
+	int i = 0;
+
+	if (!stop_flag)
+		for (i = 0; i < N; i++){
+			LOG("thread force stop");
+			kthread_stop(kthreads[i]);
+		}
+}
+
+void timer_cb(unsigned long data)
+{
+	stop_flag = 1;
+}
 
 static int mymodule_init(void)
 {
-	kthread = kthread_run(th_fun, NULL, "test thread");
+	start_threads();
+
+	setup_timer(&timer, timer_cb, T * 1000);
+	mod_timer(&timer, jiffies + msecs_to_jiffies(T * 1000));
+
 	LOG("module loaded");
 	return 0;
 }
 
 static void mymodule_exit(void)
 {
-	LOG("thread force stop");
-	kthread_stop(kthread);
+	stop_threads();
+	vfree(kthreads);
+	del_timer(&timer);
 	LOG("module exited");
 }
 
